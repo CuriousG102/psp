Fix nets.py code and change normalization code to result in smaller reconstruction weightings. Still does not solve the NaN issue :-/

dreamerv3/agent.py
@@ -443,13 +443,11 @@ class WorldModel(nj.Module):
           original_weight_sum = jnp.prod(jnp.array(weights.shape[-3:]))
           weights = (
               weights  # [L, H, W, C]
-              * (  # [L, 1, 1, 1]
-                  original_weight_sum  # [1,]
-                  / weights.sum((-1, -2, -3), keepdims=True)  # [L, 1, 1, 1]
-              )
+              / weights.sum((-1, -2, -3), keepdims=True)  # [L, 1, 1, 1]
           )
 
           # Any frame that has no weights at all is set to a uniform weighting.
+
           frame_nans = jnp.any(
               ~jnp.isfinite(weights), (-1, -2, -3), keepdims=True)  # [L, 1, 1, 1]
           weights = jnp.where(frame_nans, 1., weights)
@@ -457,7 +455,7 @@ class WorldModel(nj.Module):
           # Interpolating between gradient weighted reconstruction loss and
           # a uniform weighting.
           weights = (
-              jnp.ones_like(weights) * (
+              jnp.ones_like(weights) / original_weight_sum * (
                   1 - self.config.image_v_grad_interp_value
               )
               + weights * self.config.image_v_grad_interp_value
@@ -468,7 +466,7 @@ class WorldModel(nj.Module):
           weights = jnp.where(weights > p95, p95, weights)
 
         if self.config.image_v_grad_normed:
-          weights /= jnp.sum(weights, axis=-1, keepdims=True)
+          weights /= weights.sum((-1, -2, -3), keepdims=True)
 
         warmup = self.config.v_grad_warmup_steps
         if warmup > 0:

dreamerv3/nets.py
@@ -78,9 +78,6 @@ class RSSM(nj.Module):
       vf_post_mean = 0.
       for vf_ in vf:
         vf_post_mean_ = vf_(post).mean()
-        ndims = vf_post_mean_.ndim
-        if ndims != 1:
-          vf_post_mean_ = jnp.mean(vf_post_mean_, axis=tuple(range(1, ndims)))
         vf_post_mean += jnp.mean(vf_post_mean_)
       return vf_post_mean
 
@@ -107,7 +104,13 @@ class RSSM(nj.Module):
 
     start_post, start_prior, start_embed = state, state, embed_fn(
       jax.tree_util.tree_map(lambda x: x[0], d_data | data))
-    start_grads = jax.tree_util.tree_map(lambda x: x[0], d_data)
+    (_, _), start_grads = obs_step(
+      start_post,
+      jax.tree_util.tree_map(lambda x: x[0], d_data),
+      jax.tree_util.tree_map(lambda x: x[0], data),
+      jax.tree_util.tree_map(lambda x: x[0], action),
+      jax.tree_util.tree_map(lambda x: x[0], is_first),
+    )
     start = start_post, start_prior, start_embed, start_grads
 
     post, prior, embed, grads = jaxutils.scan(